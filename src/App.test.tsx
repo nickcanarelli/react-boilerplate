8213d85d35b6806f96a02c200e99d6e97e38111e
Nick Canarelli <ncanarelli@under.io>
2023-02-06 00:10:35 -0600

    more initial work

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-test('renders the app with a button', () => {
-  render(<App />)
-  const buttonElement = screen.getByRole('button')
-  expect(buttonElement).toBeInTheDocument()
-})
